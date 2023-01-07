d8ca18f0bf85cfebe4c8f14af3972aa1d389ddbc
Hoothoot5321 <119594665+Hoothoot5321@users.noreply.github.com>
2023-01-07 17:37:27 +0100

    sui

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
-const { Client, GatewayIntentBits, Partials, Collection } = require("discord.js");
-const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits
-const { User, Message, GuildMember, ThreadMember } = Partials
+const {
+  Client,
+  GatewayIntentBits,
+  Partials,
+  Collection,
+} = require("discord.js");
+const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits;
+const { User, Message, GuildMember, ThreadMember } = Partials;
+const { config } = require("dotenv");
+config();
+const Token = process.env.DISCORD_TOKEN;
 
-const client = new Client({ 
-    intents: [Guilds, GuildMembers, GuildMessages],
-    Partials: [User, Message, GuildMember, ThreadMember]
+const client = new Client({
+  intents: [Guilds, GuildMembers, GuildMessages],
+  Partials: [User, Message, GuildMember, ThreadMember],
 });
 
+const { loadEvents } = require("./Handlers/eventHandler");
 
-const { loadEvents } = require("./Handlers/eventHandler")
-
-client.config = require("./config.json");
 client.events = new Collection();
 client.commands = new Collection();
 client.subCommands = new Collection();
 
 loadEvents(client);
 
-
-client.login(client.config.token)
-.then(() => {
+client
+  .login(Token)
+  .then(() => {
     console.log(`Client logged in as ${client.user.tag}`);
     client.user.setActivity(`with ${client.guilds.cache.size} guilds`);
-})
-.catch((err) => console.log(err));
+  })
+  .catch((err) => console.log(err));
